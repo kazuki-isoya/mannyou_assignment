f195ed65c3cc4efcd116796e733bb89126b179d2
moru more <moremoru.world21@gmail.com>
2020-02-26 16:49:17 +0900

    タイムリミットカラムを追加した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,6 +51,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :time_limit)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_041834) do
+ActiveRecord::Schema.define(version: 2020_02_26_065446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_041834) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "time_limit", default: "2020-03-31 19:00:00", null: false
   end
 
 end
