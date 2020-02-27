e15ab6a1fabadd79e5129eca2627fc694e3c1cf9
moru more <moremoru.world21@gmail.com>
2020-02-27 12:46:56 +0900

    終了期限の入力とソート機能のテストを追記した

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
   factory :task do
     title { 'test_title' }
     content { 'test_content' }
+    time_limit { '2020-04-30 19:00:00'}
   end
 
   factory :second_task, class: Task do
     title { 'factory_second_title' }
     content { 'factory_second_content' }
+    time_limit { '2020-03-31 19:00:00'}
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,38 +2,54 @@ require 'rails_helper'
 
 RSpec.describe 'タスク管理機能', type: :system do
 
-  before do
-    @task = FactoryBot.create(:task, title: 'task')
-  end
-  describe 'タスク一覧画面' do
-    context 'タスクを作成した場合' do
-      it '作成済みのタスクが表示されること' do
-        visit tasks_path
-        expect(page).to have_content 'test'
-      end
-    end
-    context '複数のタスクを作成した場合' do
-      it 'タスクが作成日時の降順に並んでいること' do
-        new_task = FactoryBot.create(:second_task, title: 'new_task')
-        visit tasks_path
-        task_list = all('tbody tr')
-        expect(task_list[0]).to have_content 'new_task'
-        expect(task_list[1]).to have_content 'task'
-      end
-    end
-  end
 
-#   describe 'タスク登録画面' do
-#     context '必要項目を入力して、createボタンを押した場合' do
-#       it 'データが保存されること' do
-#         visit new_task_path
-#         fill_in "Title", with: 'test_title'
-#         fill_in "Content", with: 'test_content'
-#         click_button 'Create Task'
-#         expect(page).to have_content 'test'
-#       end
-#     end
-#   end
+  # before do
+  #   @task = FactoryBot.create(:task, title: 'task1')
+  #   @task2 = FactoryBot.create(:second_task, title: 'task2')
+  # end
+  # describe 'タスク一覧画面' do
+  #   context 'タスクを作成した場合' do
+  #     it '作成済みのタスクが表示されること' do
+  #       visit tasks_path
+  #       expect(page).to have_content 'test'
+  #     end
+  #   end
+  #   context '複数のタスクを作成した場合' do
+  #     it 'タスクが作成日時の降順に並んでいること' do
+  #       visit tasks_path
+  #       task_list = all('tbody tr')
+  #       expect(task_list[0]).to have_content 'task2'
+  #       expect(task_list[1]).to have_content 'task1'
+  #     end
+  #   end
+  #   context '終了期限でソートするボタンを押した場合' do
+  #     it 'タスクが終了期限の降順に並んでいること' do
+  #       task3 = FactoryBot.create(:second_task, title: 'task3', time_limit: '2020-02-28 10:00:00')
+  #       visit tasks_path
+  #       click_on '終了期限でソートする'
+  #       expect(page).to have_content 'タスク一覧'
+  #       task_list = all('tbody tr')
+  #       expect(task_list[0]).to have_content 'task1'
+  #       expect(task_list[1]).to have_content 'task2'
+  #       expect(task_list[2]).to have_content 'task3'
+  #     end
+  #   end
+  # end
+
+  describe 'タスク登録画面' do
+    context '必要項目を入力して、createボタンを押した場合' do
+      it 'データが保存されること' do
+        visit new_task_path
+        fill_in "タイトル", with: 'test_title'
+        fill_in "内容", with: 'test_content'
+        select_date("2020,3,3", from: "終了期限")
+        select_time("13", "00", from: "終了期限")
+        click_button '登録する'
+        expect(page).to have_content 'test'
+        expect(page).to have_content '2020-03-03 13:00:00'
+      end
+    end
+  end
 #
 #   describe 'タスク詳細画面' do
 #      context '任意のタスク詳細画面に遷移した場合' do
