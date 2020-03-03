86dcf594e80ce4478c79348816e31e93bb9f225e
moru more <moremoru.world21@gmail.com>
2020-03-03 14:12:51 +0900

    管理者ページと管理者ユーザーのコントローラーを作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,10 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -15,9 +21,31 @@ class Admin::UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」を更新しました"
+    else
+      render :new
+    end
+  end
+
   def show
   end
 
-  def index
+  def destroy
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました"
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+
 end
